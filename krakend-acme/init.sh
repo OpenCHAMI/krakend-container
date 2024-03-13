2ba9c0168e67c73e4f7206362d78230a48db3929
Nicholas Jones <njones@lanl.gov>
2024-03-13 15:31:59 -0600

    fixed init scripts

--- krakend-acme/init.sh
+++ krakend-acme/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 step ca bootstrap --ca-url=https://SI.ca --fingerprint=$(cat /mnt/defaults.json | jq '.fingerprint' -r) --install
-if ! [ -f /root/.acme.sh/SI.endpoint_ecc/SI.endpoint.cer  -a  -f /root/.acme.sh/SI.endpoint_ecc/SI.endpoint.key ]
+if ! [ -f /root/.acme.sh/SI.krakend_ecc/SI.krakend.cer  -a  -f /root/.acme.sh/SI.krakend_ecc/SI.krakend.key ]
 then
-  rm -rf /root/.acme.sh/SI.endpoint_ecc/
-  /root/.acme.sh/acme.sh --force --issue --standalone -d SI.endpoint --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
+  rm -rf /root/.acme.sh/SI.krakend_ecc/
+  /root/.acme.sh/acme.sh --force --issue --standalone -d SI.krakend --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
 else
-  /root/.acme.sh/acme.sh --issue --standalone -d SI.endpoint --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
+  /root/.acme.sh/acme.sh --issue --standalone -d SI.krakend --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
 fi
 update_loop.sh &
 /usr/bin/krakend run --config /etc/krakend.json
--- krakend-acme/update_loop.sh
+++ krakend-acme/update_loop.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 while true
 do	
-  if ! [ -f /root/.acme.sh/SI.endpoint_ecc/SI.endpoint.cer  -a  -f /root/.acme.sh/SI.endpoint_ecc/SI.endpoint.key ]
+  if ! [ -f /root/.acme.sh/SI.krakend_ecc/SI.krakend.cer  -a  -f /root/.acme.sh/SI.krakend_ecc/SI.krakend.key ]
   then
-    rm -rf /root/.acme.sh/SI.endpoint_ecc/
-    /root/.acme.sh/acme.sh --force --issue --standalone -d SI.endpoint --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
+    rm -rf /root/.acme.sh/SI.krakend_ecc/
+    /root/.acme.sh/acme.sh --force --issue --standalone -d SI.krakend --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
   else
-    /root/.acme.sh/acme.sh --issue --standalone -d SI.endpoint --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
+    /root/.acme.sh/acme.sh --issue --standalone -d SI.krakend --server https://SI.ca/acme/acme/directory --ca-bundle $(step path)/certs/root_ca.crt
   fi
   sleep 7200
 done
